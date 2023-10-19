d8e1bd787bc8bcd14b4c3991e8b2d707ee0edc89
George <georgeneves.dev@gmail.com>
2023-10-19 09:05:06 -0300

    feat: adicionando as classes addContract, removeContract e income.

--- src/main/java/entities/Worker.java
+++ src/main/java/entities/Worker.java
@@ -4,6 +4,7 @@ import entities.enums.WorkerLevel;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Data
@@ -23,5 +24,32 @@ public class Worker {
         this.level = level;
         this.baseSalary = baseSalary;
         this.department = department;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract){
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month){
+
+        double sum = baseSalary;
+        Calendar cal = Calendar.getInstance();
+
+        for(HourContract c: contracts){
+
+            cal.setTime(c.getDate());
+
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = cal.get(Calendar.MONTH);
+
+            if (year == c_year &&  month == c_month){
+                sum += c.totalValue();
+            }
+        }
+        return sum;
     }
 }

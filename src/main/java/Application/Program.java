ed5e1b0bdd71b6a0bd08fc7cca0565a7b168895d
George <georgeneves.dev@gmail.com>
2023-10-19 09:20:21 -0300

    feat: adicionando inputs com o Scanner na classe principal

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -1,5 +1,12 @@
 package Application;
 
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,20 @@ public class Program
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+
+        System.out.print("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base Salary: ");
+        double baseSalary= sc.nextDouble();
+
+        sc.close();
     }
 }
